73f8b4b1c8000633687f23e50473fee954a83b64
Patrick <gather4real@yahoo.com>
2020-10-17 06:03:03 +0000

    Integration of flight+hoteljs on index page

---
+++ assets/js/hotelflight.js
@@ -0,0 +1,50 @@
+// function for getting document by id used in form input value 
+function _(x){
+	return document.querySelector(x);
+}
+
+        // search hotel and flight button click listener
+        _("#sBtn").addEventListener("click", function() {
+            //  Book flight form inputs
+              var where = _("#where").value;
+              var destination = _("#destination").value;
+              var departure = _("#departure").value;
+              var room = _("#room").value;
+              
+              console.log('hotel and flight clicked');
+  
+              var url = 'https://a76dcb3d-31ea-42af-8ff3-5c4a21786b16.mock.pstmn.io/hotelflight';
+              
+              // get class id for showing loading notification
+              document.getElementById('status').style.display = 'inline';
+  
+              // check if required input is not empty
+              if (where == '' || destination == '' || departure == '' || room == '') {
+                  alert("All fields required");
+                  document.getElementById('status').style.display = 'none';
+                  return false;
+              }
+             
+  
+              // begin get request
+              fetch(url, {
+                  method: 'GET',
+                  headers: new Headers({
+                      'Content-Type': 'application/json',
+                      // 'AUTHORIZATION': 'Bearer '+ token
+                  })
+              }).then(res => res.json()).catch(error => console.log('Error:', error)).then(response => {
+                
+                document.getElementById('status').style.display = 'none';
+               
+                console.log(response);
+                 
+              // add response to result variable
+                var result = response;
+  
+                // inject response data into html table
+                document.getElementById('hf').insertAdjacentHTML("beforeend", "<tr><td>" + result.hotel + " </td><td>" + result.address + "</td><td>" + result.phone +  "</td><td>" + result.flightnb +  "</td><td>" + result.carrier + "</td><td>"  + result.terminal + "</td></tr>" )
+                    
+  
+              })
+          });
