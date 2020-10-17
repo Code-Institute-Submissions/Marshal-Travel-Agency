0a75856ba4c2a2dec0a33bba1b1e0a8c5332b4d9
Patrick <gather4real@yahoo.com>
2020-10-17 01:34:47 +0000

    Integration of hoteljs on index page

--- assets/js/hotel.js
+++ assets/js/hotel.js
@@ -1,85 +1,64 @@
-/*document.getElementById("search_hotel").addEventListener
-('click', search_hotel);
-document.getElementById("addHotel").addEventListener
-('submit', addHotel);
-
-function search_hotel(){
-    fetch("https://hotels4.p.rapidapi.com/properties/get-details?locale=en_US&currency=USD&checkOut=2020-01-15&adults1=1&checkIn=2020-01-08&id=424023",{
-    "method": "GET",
-	"headers": {
-		"x-rapidapi-host": "hotels4.p.rapidapi.com",
-		"x-rapidapi-key": "6a06979eecmshb360e05ef335a2fp1faf3fjsn89f3c1a6a34b"
-	}
-})
-    
-    .then((res) => res.json())
-    .then((data) => {
-        let result = "<h2>Hotels</h2>";
-        data.forEach(function(hotel){
-            result += `
-                <div>
-                    <h3>${hotel.result}</h3>
-                    <p>${hotel.data.body}</p>
-                    <p>${hotel.body}</p>
-                    <p>${data.transportation}</p>
-                    <p>${data.neighborhood}</p>
-                </div>
-
-            `;
-        });
-         document.getElementById("result").innerHTML = result;
-    })
-}
-
-function addHotel(e){
-    e.preventDefault();
-
-    let result = document.getElementById("result").value;
-    let data = document.getElementById("data").value;
-    let body = document.getElementById("body").value;
-
-    fetch("https://hotels4.p.rapidapi.com/properties/get-details?locale=en_US&currency=USD&checkOut=2020-01-15&adults1=1&checkIn=2020-01-08&id=424023",{
-        "method": "GET",
-	    "headers": {
-		    "x-rapidapi-host": "hotels4.p.rapidapi.com",
-		    "x-rapidapi-key": "6a06979eecmshb360e05ef335a2fp1faf3fjsn89f3c1a6a34b"
-	    },
-        body:JSON.stringify({result:result, data:data, body:body})
-    })
-    .then((res) => res.json())
-    .then((data) => console.log(data))
-}
-*/
-        
- 
-       
-
-document.getElementById("search_hotel").addEventListener
-('click', search_hotel);
-document.getElementById("addHotel").addEventListener
-('submit', addHotel);
-
-function search_hotel(){
-    fetch("user.json")
-    
-    .then((res) => res.json())
-    .then((data) => {
-        let result = "<h2>Hotels</h2>";
-        console.log(data);
-        data.forEach(function(hotel){
-            result += `
-                <ul>
-                    <li>ID: ${hotel.id}</li>
-                    <li>Name: ${hotel.name}</li>
-                    <li>Email: ${hotel.email}</li>
-                    
-                </ul>
-
-            `;
-        });
-        document.getElementById("result").innerHTML = result;
-    })
+// function for getting document by id used in form input value 
+function _(x){
+	return document.querySelector(x);
 }
 
 
-    
+    // search hotel button click listener
+    _("#hBtn").addEventListener("click", function() {
+        //  Book hotel form inputs
+          var location = _("#loc").value;
+         
+          
+          console.log(location)
+
+          // get API access token from localstorag
+          var token = localStorage.getItem("token");
+
+          var url = 'https://test.api.amadeus.com/v2/shopping/hotel-offers?cityCode='+ location;
+         
+          // get class id for showing loading notification
+          document.getElementById('status').style.display = 'inline';
+
+          // check if required input is not empty
+          if (location == '' ) {
+              
+            alert("All fields required");
+            document.getElementById('status').style.display = 'none';
+            return false;
+          }
+         
+          
+
+          // begin get request
+          fetch(url, {
+              method: 'GET',
+              headers: new Headers({
+                  'Content-Type': 'application/json',
+                  'AUTHORIZATION': 'Bearer '+ token
+              })
+          }).then(res => res.json()).catch(error => console.log('Error:', error)).then(response => {
+
+            document.getElementById('status').style.display = 'none';
+
+            console.log(response);
+             
+          // for loop in response object and push all object into result variable
+            var result = [];
+            var d = [];
+            d= response.data;
+            for (var i in d) {
+            d[i].id = i;
+             result.push(d[i]);
+              // console.log(d[i]);
+            }
+              
+          // for loop objects in result variable and inject into html table
+            for (i in result){
+            
+              document.getElementById('hotel').insertAdjacentHTML("beforeend", "<tr><td>" + result[i].offers[0].checkInDate + " </td><td>" + result[i].offers[0].checkOutDate + "</td><td>" + result[i].offers[0].price.total + "</td><td>"  + result[i].hotel.name + "</td><td>" +  result[i].hotel.address.cityName + "</td>"  + "<td>" + result[i].hotel.address.lines[0] + "</td><td>"   + result[i].hotel.contact.phone + "</td></tr>" )
+            
+          }
+
+        })
+      });
