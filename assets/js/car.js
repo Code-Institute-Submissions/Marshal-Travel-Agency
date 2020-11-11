8d4813044e4b75102a58cc92c859091a72cb91ec
Patrick <gather4real@yahoo.com>
2020-11-11 09:35:08 +0000

    Merged back-to-top-button with main.js

--- assets/js/car.js
+++ assets/js/car.js
@@ -76,6 +76,6 @@ var result = JSON.parse(localStorage.getItem("car"));
 for (i in result) {
 	x = result[i];
 	// inject response data into html table
-	document.getElementById('ch').insertAdjacentHTML("beforeend", "<tr><td>" + x.pickup + " </td><td>" + x.Location + "</td><td>" + x.car + "</td><td>" + x.type + "</td>" + "<td>" + x.time + "</td><td>" + x.drop + "</td></tr>");
+	document.getElementById('ch').insertAdjacentHTML("beforeend", "<tr><td>" + x.pickup + " </td><td>" + x.Location + "</td><td>" + x.car + "</td><td>" + x.type + "</td>" + "<td>" + x.time + "</td><td>" + x.drop + "</td></tr>")
 }
 
