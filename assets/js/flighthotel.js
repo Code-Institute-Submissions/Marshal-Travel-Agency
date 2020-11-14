b2f1c02084b9ce8999b7e45bf4b86e9802057b7a
Patrick <gather4real@yahoo.com>
2020-11-14 11:45:12 +0000

    arrangement of redirecting script on bottom page

--- assets/js/flighthotel.js
+++ assets/js/flighthotel.js
@@ -79,10 +79,10 @@ _("#sBtn").addEventListener("click", function () {
 	});
 });
 
-// fetch flight and hotel data from localstorage
+/*// fetch flight and hotel data from localstorage
 var result = JSON.parse(localStorage.getItem("FandHdata"));
 for (i in result) {
 	x = result[i];
 	// inject response data into html table
 	document.getElementById('hf').insertAdjacentHTML("beforeend", "<tr><td>" + x.hotel + " </td><td>" + x.address + "</td><td>" + x.phone + "</td><td>" + x.flightnb + "</td><td>" + x.carrier + "</td><td>" + x.terminal + "</td><td>" + x.price + "</td></tr>")
-}
+}*/
--- assets/js/hotel.js
+++ assets/js/hotel.js
@@ -92,9 +92,9 @@ _("#hBtn").addEventListener("click", function () {
 		window.location = './hotel.html';
 	});
 });
-// fetch hotel data from localstorage
+/*// fetch hotel data from localstorage
 var result = JSON.parse(localStorage.getItem("hoteldata"));
 // for loop objects in result variable and inject into html table
 for (i in result) {
-	document.getElementById('hotel').insertAdjacentHTML("beforeend", "<tr><td>" + result[i].offers[0].checkInDate + " </td><td>" + result[i].offers[0].checkOutDate + "</td><td>" + result[i].offers[0].price.total + "</td><td>" + result[i].hotel.name + "</td><td>" + result[i].hotel.address.cityName + "</td>" + "<td>" + result[i].hotel.address.lines[0] + "</td><td>" + result[i].hotel.contact.phone + "</td></tr>");
-}
+	document.getElementById('hotel').insertAdjacentHTML("beforeend", "<tr><td>" + result[i].offers[0].checkInDate + " </th><td>" + result[i].offers[0].checkOutDate + "</td><td>" + result[i].offers[0].price.total + "</td><td>" + result[i].hotel.name + "</td><td>" + result[i].hotel.address.cityName + "</td>" + "<td>" + result[i].hotel.address.lines[0] + "</td><td>" + result[i].hotel.contact.phone + "</td></tr>");
+}*/
