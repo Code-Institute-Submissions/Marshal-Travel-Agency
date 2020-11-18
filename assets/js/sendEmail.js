ab54dc058d5b0ea06daf64e601c8ea6ace3c96bc
Patrick <gather4real@yahoo.com>
2020-11-18 17:34:41 +0000

    Added modal validation for contact form

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,42 +1,40 @@
 function sendMail(contactForm) {
-    emailjs.send("marshal", "template_tYlG7zl9", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
-        "phone_number": contactForm.phonenumber.value,
-        "booking_request": contactForm.bookingsummary.value
-    })
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-        },
-        function(error) {
-            console.log("FAILED", error);
-        }
-    );
-    document.getElementById('contactForm').reset();
-    return false;  // To block from loading a new page
+	emailjs.send("marshal", "template_tYlG7zl9", {
+		"from_name": contactForm.name.value,
+		"from_email": contactForm.emailaddress.value,
+		"phone_number": contactForm.phonenumber.value,
+		"booking_request": contactForm.bookingsummary.value
+	}).then(function (response) {
+		console.log("SUCCESS", response);
+	}, function (error) {
+		console.log("FAILED", error);
+	});
+	document.getElementById('contactForm').reset();
+	return false; // To block from loading a new page
 }
-
-var fname = _("#fname").value;
-var phone = _("#phone").value;
-var email = _("#email").value;
-var message = _("#message").value;
-
-// Conditions
-function mySub() {
-    var x = document.getElementById("Submit");
-    
-if (fname == '' || phone == '' || email == '' || message == '')  {
-    alert("All fields are required");
-    subtn.onclick = function() {
-    x.style.display = "none";
-}
-    
- 	// disable loading
-    x.style.display = "false";
-    return false;
-} else {
-    x.style.display = "block";
- 	return true;
-};
-};
+//validation of modal pop-up
+$(document).on("click", "#submit", function () {
+	var fname = $("#fname").val();
+	var phone = $("#phone").val();
+	var email = $("#email").val();
+	var message = $("#message").val();
+	if (fname == "") {
+		alert("Full Name Required");
+		return false;
+	} else if (phone == "") {
+		alert("Phone number Required");
+		return false;
+	} else if (email == "") {
+		alert("Email Required");
+		return false;
+	} else if (message == "") {
+		alert("Request Required");
+		return false;
+	} else {
+		$("#Submit").modal("show");
+		$('#fname_modal').text(fname);
+		$('#phone_modal').text(phone);
+		$('#email_modal').text(email);
+		$('#message_modal').text(message);
+	}
+});
